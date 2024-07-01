d878fe1e7fbbf8c0e67e4958470f7f0799988b45
shah010101 <97405957+shah010101@users.noreply.github.com>
2024-07-02 00:10:14 +0530

    Update orderController.js one '/' was replaced

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from frontend 
 const placeOrder=async(req,res)=>{
-    const frontend_url="https://food-circuit-frontend.onrender.com/";
+    const frontend_url="https://food-circuit-frontend.onrender.com";
 
 
     try{
