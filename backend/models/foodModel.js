0ca293c260adb7b1d304c9c72a5f37e8413ac97c
shah010101 <97405957+shah010101@users.noreply.github.com>
2024-07-01 19:58:46 +0530

    first-commit

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -14,7 +14,7 @@ const foodSchema= new mongoose.Schema({
 
 
 // here one important point is take care of is the model not creted again so that || is used if it is exits already then it will not created again
-const foodModel=mongoose.models.food|| mongoose.model("food",foodSchema);
+const foodModel=mongoose.models.food||mongoose.model("food",foodSchema);
 
 
 export default foodModel;
